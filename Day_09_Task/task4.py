4c45e6e53a172a534b21556b28edf14c4034b2c6
Afnankumbari <158656524+Afnankumbari@users.noreply.github.com>
2026-02-15 21:28:03 +0530

    Delete Day_09_Task directory

--- Day_09_Task/task4.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-prices = np.array([100, 200, 300, 400])
-tax = 0.18   # 18% GST
-
-# broadcasting
-final_price = prices + (prices * tax)
-print("Final prices after tax:", final_price)
-
